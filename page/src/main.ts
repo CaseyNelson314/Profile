4970eb16479389337cb4afbf3346fda63c851968
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2024-05-20 17:32:22 +0900

    update

--- page/src/main.ts
+++ page/src/main.ts
@@ -1,1 +1,9 @@
 import './style.css'
+import $ from 'jquery'
+import 'slick-carousel'
+
+$('.project_image').slick({
+    autoplay: true, // 自動再生
+    arrows: true, // 矢印
+    dots: true, // インジケーター
+});
